e02e87f2ea47c76e7bdc428de54d493d63549bca
c-santos <jcms121899@gmail.com>
2024-06-16 13:59:41 +0800

    Added TypeormModule import to ProductModule

--- src/infrastructure/modules/product.module.ts
+++ src/infrastructure/modules/product.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductRepository } from '../database/repositories/product.repository';
 import { ProductService } from '@/application/services/product.service';
 import { ProductController } from '../http/controllers/product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from '../database/models/product.model';
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([Product])],
     providers: [ProductRepository, ProductService],
     controllers: [ProductController],
     exports: [],
