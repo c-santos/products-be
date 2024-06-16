414b1099406574c5c7567dbc750b38dff35e2b85
c-santos <jcms121899@gmail.com>
2024-06-16 14:13:57 +0800

    Fixed files

--- setup-local.sh
+++ setup-local.sh
@@ -9,13 +9,13 @@ $DC down
 
 # Spin up conatiners
 $DC build --no-cache
-$DC run --rm my-template npm install
+$DC run --rm products-be npm install
 $DC up -d
 
 # Run migration and seeders
 $DC exec my-template npm run typeorm:migration:generate --name=Init
 $DC exec my-template npm run typeorm:migration:run
-# $DC exec my-template npm run typeorm:seed
+$DC exec my-template npm run typeorm:seed
 
 # show logs
 $DC logs -f
